8dc7f070339c63b660195cd9400041db9cb95a5e
lyw1217 <mvl100d@gmail.com>
2022-03-15 10:58:42 +0900

    동영상 길이 구하기

---
+++ getDuration.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#if [ $# -eq 0 ]; then
+#	echo "No arguments supplied"
+#fi
+VIDEO_PATH="/home/leeyw/mnt/Twitch/recordings"
+#for arg in "$@"
+for arg in "$VIDEO_PATH"/*
+do
+	if [ ! -e $arg ]; then
+		echo "File $arg is not exists."
+		continue
+	fi
+	
+	fileext=${arg##*.}
+	if [ "$fileext" != "ts" ]; then
+		continue
+	fi
+	
+	echo $arg
+
+	ffmpeg -i $arg 2>&1 | grep Duration | awk '{print $w}'
+	echo
+done
