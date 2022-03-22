33e1af3e730ffcfb01f30b237fbe7be379a1b9d4
lyw1217 <mvl100d@gmail.com>
2022-03-22 09:46:10 +0900

    디렉토리 전체탐색하도록 변경

--- cutVideo.sh
+++ cutVideo.sh
@@ -1,24 +1,55 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]; then
-	echo "Usage : $0 {input_file.ts}"
-	exit 0
-fi
 
-full_name=$1
-echo "input file name = ${full_name}"
+#if [ $# -lt 1 ]; then
+#	echo ""
+#	echo "Usage : $0 {input_file.ts}"
+#	echo ""
+#	exit 0
+#fi
 
-ext="${full_name:(-3)}"
-name="${full_name:0:(-3)}"
+VIDEO_PATH="/home/leeyw/mnt/Twitch/recordings"
 
-if [ "$ext" != ".ts" ]; then
-	echo "Only .ts file"
-	exit 0
-fi
+for arg in "$VIDEO_PATH"/*
+do
+	if [ ! -e "$arg" ]; then
+		echo "File "$arg" is not exists."
+		continue
+	fi
 
-echo "cut video, [start ~ 06:00:00 ] / [ 06:00:00 ~ end ]"
+	full_name=$arg
+	ext="${full_name:(-3)}"
+	name="${full_name:0:(-3)}"
 
-ffmpeg -i ${full_name} -t 06:00:00 -c:v copy -c:a copy ${name}_1.ts
-ffmpeg -i ${full_name} -ss 06:00:00 -c:v copy -c:a copy ${name}_2.ts
+	if [ "$ext" != ".ts" ]; then
+		#echo ""
+		#echo "Only .ts file"
+		#echo ""
+		continue
+	fi
+	
+	echo ""
+	echo "Input file name = ${full_name}"
+
+	duration=$(ffmpeg -i $arg 2>&1 | grep Duration | cut -d ' ' -f 4 | sed s/,//)
+	echo "Duration : ${duration}"
+
+	if [ "${duration:0:2}" -lt "12" ]; then
+		echo ""
+		echo "This Video is shorter than 12 hours. Just Upload it."
+		echo ""
+		continue
+	fi
+
+	echo ""
+	echo "cut video, [00:00:00 ~ 06:00:00 ] / [ 06:00:00 ~ ${duration} ]"
+
+	#ffmpeg -i ${full_name} -ss 00:00:00 -to 06:00:02 -c:v copy -c:a copy ${name}_1.ts
+	ffmpeg -i ${full_name} -to 06:00:02 -c:v copy -c:a copy ${name}_1.ts
+	sleep 5
+	#ffmpeg -i ${full_name} -ss 05:59:58 -to ${duration} -c:v copy -c:a copy ${name}_2.ts
+	ffmpeg -i ${full_name} -ss 05:59:58 -c:v copy -c:a copy ${name}_2.ts
+
+done
 
 echo "end.."
