4ba1b3fd75c7cea41e1041dab14ccafb6bcea006
lyw1217 <mvl100d@gmail.com>
2022-04-07 11:39:41 +0900

    하루 전 영상들 다운받는 스크립트

---
+++ downVideo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+VIDEO_PATH="${HOME}/mnt/Twitch/recordings"
+TARGET_URLS=()
+
+while read url; do
+    if [ -z "$url" ]; then continue; fi
+    TARGET_URLS+=($url)
+done < /home/ubuntu/Documents/github/streamlink-live-download/target_url.txt
+
+for arg in "${TARGET_URLS[@]}"
+do
+    echo ""
+    echo "download start!!!"
+	echo $arg
+    
+	youtube-dl -f bestvideo+bestaudio/best --limit-rate 8M --playlist-end 1 --buffer-size 16K --dateafter now-1day -o "${VIDEO_PATH}/%(uploader)s %(upload_date)s %(title)s.%(ext)s" "${arg}/videos"
+    
+    echo "download complete!!!"
+    echo ""
+    sleep 5
+done
+
