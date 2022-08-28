f320708faa654b97f5cc473cfaa1a9b319794da4
lyw1217 <mvl100d@gmail.com>
2022-08-28 12:47:29 +0900

    youtube-dl 에서 yt-dlp 로 변경

--- scripts/downVideo.sh
+++ scripts/downVideo.sh
@@ -3,13 +3,19 @@
 # 하루 전 영상을 다운로드받는 스크립트
 
 VIDEO_PATH="${HOME}/mnt/Twitch/recordings"
-INPUT="/home/ubuntu/Documents/github/streamlink-live-download/download_url.txt"
+INPUT="${HOME}/Documents/github/streamlink-live-download/download_url.txt"
 TARGET_URLS=()
-PROGRAM="youtube-dl"
+PROGRAM="yt-dlp"
 CMD=`command -v ${PROGRAM} 2>/dev/null`
 
 if [ ! -d ${VIDEO_PATH} ]; then
     echo "'${VIDEO_PATH}' Directory does not exist!"
+    echo "exit"
+    exit
+fi
+
+if [ ! -e ${INPUT} ]; then
+    echo "'${INPUT}' File does not exist!"
     echo "exit"
     exit
 fi
@@ -21,10 +27,14 @@ if [ -z "$CMD" ]; then
     exit
 fi 
 
+echo "===START DOWNLOAD SCRIPT==="
+
 while read url; do
     if [ -z "$url" ]; then continue; fi
     TARGET_URLS+=($url)
 done < $INPUT
+
+echo "TARGET_URLS = ${TARGET_URLS}"
 
 for arg in "${TARGET_URLS[@]}"
 do
@@ -32,10 +42,11 @@ do
     echo "download start!!!"
 	echo $arg
     
-	$CMD -f bestvideo+bestaudio/best --playlist-end 5 --buffer-size 32K --dateafter now-1day -o "${VIDEO_PATH}/%(uploader)s_%(upload_date)s_%(title)s.%(ext)s" "${arg}/videos"
+	$CMD --verbose -f bestvideo*+bestaudio/best --throttled-rate 100k --playlist-end 5 --dateafter now-1day -o "${VIDEO_PATH}/[%(uploader)s]_%(upload_date)s_%(title)s.%(ext)s" "${arg}/videos"
     
     echo "download complete!!!"
     echo ""
     sleep 5
 done
 
+echo "=== END  DOWNLOAD SCRIPT==="
